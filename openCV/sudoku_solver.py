42ceaf670828b1c8a92dc387f70f38f49865a4d2
Romain de Spoelberch <rdespoelberch@yahoo.com>
2019-05-11 16:43:40 +0200

    refactored to make names consistent

--- openCV/sudoku_solver.py
+++ openCV/sudoku_solver.py
@@ -1,27 +1,27 @@
 
 
-def solve_board(board):
+def solve_grid(grid):
     def check_box(row, col, num):
         row = row - (row % 3)
         col = col - (col % 3)
     
         for x in range(0, 3):
             for y in range (0, 3):
-                if board[row + x][col + y] == num:
+                if grid[row + x][col + y] == num:
                     return False
         return True
     
 
     def check_col(col, num):
         for row in range(9):
-            if board[row][col] == num:
+            if grid[row][col] == num:
                 return False
         return True
     
 
     def check_row(row, num):
         for col in range(9):
-            if board[row][col] == num:
+            if grid[row][col] == num:
                 return False
         return True
 
@@ -38,27 +38,27 @@ def solve_board(board):
 
     def solve(row, col):
         if row > 8 :
-            print(board)
-            return board
-        elif board[row][col] != 0:
+            print(grid)
+            return grid
+        elif grid[row][col] != 0:
             solve_next(row, col)
         else:
             for num in range(1, 10):
                 if do_checks(row, col, num):
-                    board[row][col] = num
+                    grid[row][col] = num
                     solve_next(row, col)
-            board[row][col] = 0
+            grid[row][col] = 0
 
     return solve(0,0)
 
     
-def turn_into_board(arr):
-    board = [[arr[x+9*y] for x in range(9)] for y in range(9)]
-    return board
+def turn_into_grid(arr):
+    grid = [[arr[x+9*y] for x in range(9)] for y in range(9)]
+    return grid
 
 
 def main():
-    board = turn_into_board([0, 0, 0, 6, 0, 4, 7, 0, 0, 
+    grid = turn_into_grid([0, 0, 0, 6, 0, 4, 7, 0, 0, 
                              7, 0, 6, 0, 0, 0, 0, 0, 9,
                              0, 0, 0, 0, 0, 5, 0, 8, 0,
                              0, 7, 0, 0, 2, 0, 0, 9, 3,
@@ -67,7 +67,7 @@ def main():
                              0, 5, 0, 2, 0, 0, 0, 0, 0,
                              3, 0, 0, 0, 0, 0, 2, 0, 8,
                              0, 0, 2, 3, 0, 1, 0, 0, 0])
-    board = solve_board(board)
+    grid = solve_grid(grid)
 
 
 if __name__ == '__main__':
